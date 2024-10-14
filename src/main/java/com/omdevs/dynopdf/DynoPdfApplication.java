9662c88ef104ee07c602abc1f50486c1bffc3015
Omdev Shastri <omdevshastriwork@gmail.com>
2024-10-14 10:43:23 +0530

    init

---
+++ src/main/java/com/omdevs/dynopdf/DynoPdfApplication.java
@@ -0,0 +1,13 @@
+package com.omdevs.dynopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynoPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynoPdfApplication.class, args);
+    }
+
+}
